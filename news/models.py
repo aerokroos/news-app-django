21e9bb3de77b7c7f28fb1e29902387ef19ec76f5
jalbertoornelass <albertoornelass44@gmail.com>
2020-08-21 17:40:24 -0700

    adding footer

--- news/models.py
+++ news/models.py
@@ -1,21 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Reporter(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='reporters/',null=True, blank=True)
     website = models.CharField(max_length=100)
     bio = models.TextField()
+
+    # def __str__(self):
+    #     return self.first_name.title() + " " + self.last_name.title()
     
-    def __str__(self):
-        return self.first_name.title() + " " + self.last_name.title()
-    
-    @property
-    def full_name(self):
-        full_name = self.first_name.title() + " " + self.last_name.title()
-        return full_name
+    # @property
+    # def full_name(self):
+    #     full_name = self.first_name.title() + " " + self.last_name.title()
+    #     return full_name
 
 class Section(models.Model):
     name_section = models.CharField(max_length=30)
--- newsapp/urls.py
+++ newsapp/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('news.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 # images
