4224d457268b8efa2c0787313503717093535e4f
jalbertoornelass <albertoornelass44@gmail.com>
2020-08-24 17:48:24 -0700

    migratios comment

--- news/models.py
+++ news/models.py
@@ -36,7 +36,7 @@ class Article(models.Model):
     def __str__(self):
         return self.title
 
-class Comments(models.Model):
+class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
